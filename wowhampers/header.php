b57524a718b4deae24a58673418c25575ed17455
YuraPaisanidi <yura.paisanidi@gmail.com>
2021-10-07 13:38:55 +0300

    404

--- wowhampers/header.php
+++ wowhampers/header.php
@@ -16,7 +16,7 @@
 	<head>
 
 		<meta charset="utf-8">
-		<title><?php the_title(); ?></title>
+		<title><?php wp_title(); ?></title>
 		<meta name="viewport" content="width=device-width">
 		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.ico">
 		<meta property="og:image" content="img/preview.jpg">
