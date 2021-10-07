14def07da556c674b951a2ae6a6ce231ce393100
YuraPaisanidi <yura.paisanidi@gmail.com>
2021-10-07 14:01:22 +0300

    privacy + corporate

--- wowhampers/page.php
+++ wowhampers/page.php
@@ -1,38 +1,27 @@
 <?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package wowHampers
- */
-
+	/**
+		* Template name: Privacy policy
+	*/
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main class="main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+		<section class="policy">
+			<div class="policy__container container">
+		
+					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					
+						<h1 class="h1"><?php the_title(); ?></h1>
+	
+						<?php the_content(); ?>
+	
+					<?php endwhile; ?>
+					<?php endif; ?>
+	
+			</div>
+		</section>
 
-			get_template_part( 'template-parts/content', 'page' );
+	</main>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
-get_footer();
+<?php get_footer(); ?>
